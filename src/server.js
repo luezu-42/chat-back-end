10d2dc897e14a54436dcddc5eda1808d54a332a1
luezu-42 <leonardouezu@gmail.com>
2021-03-03 06:02:16 -0300

    teste

--- src/server.js
+++ src/server.js
@@ -3,9 +3,11 @@ const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const dotenv = require("dotenv");
+
 dotenv.config();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3333
+
 const server = app.listen(port , () => {
   console.log("Servidor rodando na porta " + port);
 });
